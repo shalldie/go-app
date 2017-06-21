196f6424b1d0c2ed0a54cc073136e906f96655e0
shalldie <394229785@qq.com>
2017-06-21 19:14:39 +0800

    add list file

---
+++ src/components/SelectList.vue
@@ -0,0 +1,47 @@
+<template>
+    <ul @touchmove="onTouchMove" class="select-list">
+        <li class="select-list-item"></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        list: {
+            required: true,
+            type: Array
+        }
+    },
+    data() {
+        return {
+            touchs: []
+        };
+    },
+    methods: {
+        onTouchStart(ex) {
+            this.touches[0] = this.touches[1] = ex.touches[0];
+        },
+        onTouchMove(ex) {
+            // 保存位置
+            this.touchs[0] = this.touchs[1];
+            this.touchs[1] = {
+                time: +new Date,
+                pageY: ex.touches[0].pageY
+            };
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.select-list {
+    position: relative;
+    width: 100%;
+    height: 238px;
+    box-sizing: border-box;
+
+    &-item {
+        box-sizing: border-box;
+    }
+}
+</style>
